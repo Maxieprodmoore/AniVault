2514ef912f37445555473a82e563bb6cfcb0614b
Maxieprodmoore <binglinho@gmail.com>
2021-10-07 23:09:18 +0800

    tackled slides nav buttons issue

---
+++ dist/shoelace/icon/library.js
@@ -0,0 +1,18 @@
+import {
+  getIconLibrary,
+  registerIconLibrary,
+  unregisterIconLibrary,
+  unwatchIcon,
+  watchIcon
+} from "../../chunks/chunk.KN7ZMK6O.js";
+import "../../chunks/chunk.73PEGQ4K.js";
+import "../../chunks/chunk.FGIYSBZ6.js";
+import "../../chunks/chunk.S6TJZ6NJ.js";
+import "../../chunks/chunk.IHGPZX35.js";
+export {
+  getIconLibrary,
+  registerIconLibrary,
+  unregisterIconLibrary,
+  unwatchIcon,
+  watchIcon
+};
