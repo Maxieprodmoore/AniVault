b0a238cc10a3071b305bafc0346b3476d02e4afc
Maxieprodmoore <binglinho@gmail.com>
2021-10-12 14:09:42 +0800

    Started filter option for a section

--- dist/index.ff151065.js
+++ dist/index.ff151065.js
@@ -466,11 +466,28 @@ function openCity(cityName, elmnt, color) {
 }
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
-/*======================Product Filtering================*/ tabcontent = document.getElementsByClassName("tabcontent");
-for(i = 0; i < tabcontent.length; i++)tabcontent[i].style.display = "none";
-tablinks = document.getElementsByClassName("tablink");
-for(i = 0; i < tablinks.length; i++)tablinks[i].style.backgroundColor = "";
-document.getElementById(cityName).style.display = "block";
-elmnt.style.backgroundColor = color;
+/*======================Product Filtering================*/ filterSelection("all");
+function filterSelection(c) {
+    var x, i;
+    /* Get products -------------------------*/ x = document.getElementsByClassName("productDiv");
+    if (c == "all") c = "";
+    for(i = 0; i < x.length; i++){
+        sectionRemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) sectionAddClass(x[i], "show");
+    }
+}
+/* Show Elements---------------------*/ function sectionAddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for(i = 0; i < arr2.length; i++)if (arr1.indexOf(arr2[i]) == -1) element.className += " " + arr2[i];
+}
+/* Hide elements---------------------*/ function sectionRemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for(i = 0; i < arr2.length; i++)while(arr1.indexOf(arr2[i]) > -1)arr1.splice(arr1.indexOf(arr2[i]), 1);
+    element.className = arr1.join(" ");
+}
 
 //# sourceMappingURL=index.ff151065.js.map
--- js/tabs.js
+++ js/tabs.js
@@ -17,13 +17,37 @@ function openCity(cityName,elmnt,color) {
 document.getElementById("defaultOpen").click();
 
 /*======================Product Filtering================*/
-tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  /* Get products -------------------------*/
+  x = document.getElementsByClassName("productDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    sectionRemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) sectionAddClass(x[i], "show");
   }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
+}
+
+/* Show Elements---------------------*/
+function sectionAddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++ ){
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
   }
-  document.getElementById(cityName).style.display = "block";
-  elmnt.style.backgroundColor = color;
+}
+
+/* Hide elements---------------------*/
+function sectionRemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++ ){
+    while (arr1.indexOf(arr2[i]) > -1){
+      arr1.splice(arr1.indexOf(arr2[i]), 1)
+    }
+  }
+  element.className = arr1.join(" ");
+}
